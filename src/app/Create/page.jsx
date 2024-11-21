6bb100c2a50732dc7f923f3e66d8a10276f8caed
Shivansh-Bhatnagar1207 <Shivanshb55@gmail.com>
2024-11-21 12:01:58 +0530

    prisma

--- src/app/Create/page.jsx
+++ src/app/Create/page.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Create() {
-  return <div>Enter Student Details</div>;
+  return (
+    <>
+      <div className="text-center text-4xl font-bold py-4">
+        Enter Student Details
+      </div>
+      <form>
+        <lable>Name</lable>
+      </form>
+    </>
+  );
 }
